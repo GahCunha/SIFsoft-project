4fe4bbf0fbb803794d9c42009c530cd7fccba622
Luiz Felipe Pereira de Lima <150875264+LuizFelipeLP@users.noreply.github.com>
2025-12-03 03:46:14 +0000

    menu e contato feitos

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -2,8 +2,41 @@ import { Section } from "../commum/section";
 
 export function Contact() {
   return (
-    <Section className="" center id="contact">
-      <h2 className="font-serif text-3xl text-black italic">Contact</h2>
+    <Section
+      id="contact" center className="relative min-h-[calc(100vh-4rem)] bg-black text-white">
+      <div className="z-10 grid w-full max-w-6xl grid-cols-1 gap-10 px-6 md:grid-cols-3">
+        <div>
+          <h1 className="font-lily text-5xl text-white-400">Quibeez</h1>
+          <h2 className="font-poppins text-xl text-amber font-light uppercase tracking-widest">Restaurant</h2>
+          <p className="mt-6 text-sm leading-relaxed text-gray-300">
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old.</p>
+          <div className="mt-6 flex gap-4 text-xl">
+            <i className="fab fa-facebook"></i>
+            <i className="fab fa-twitter"></i>
+            <i className="fab fa-instagram"></i>
+            <i className="fab fa-youtube"></i>
+            <i className="fab fa-pinterest"></i>
+          </div>
+        </div>
+        <div>
+          <h3 className="mb-4 text-xl font-semibold text-amber-400">
+            Location</h3>
+          <p className="text-gray-300">
+            Lorem <br />
+            Lorem ipsum</p>
+        </div>
+        <div>
+          <h3 className="mb-4 text-xl font-semibold text-amber-400">Contact Us</h3>
+          <p className="text-gray-300">
+            +91 2222 333 444 <br />
+            quibeez@example.com
+          </p>
+        </div>
+      </div>
+      <div className="absolute inset-10 z-0 rounded-4xl bg-[url('/src/assets/Rectangle 20.png')] bg-cover bg-center bg-no-repeat opacity-30"></div>
+      <div className="absolute bottom-0 z-10 w-full border-t border-white/20 py-4 text-center text-sm text-gray-300">Copyright © 2023 Quibeez</div>
     </Section>
   );
 }
--- src/components/sections/menu.tsx
+++ src/components/sections/menu.tsx
@@ -1,9 +1,31 @@
 import { Section } from "../commum/section";
 
 export function Menu() {
+
+   const items = [
+    { name: "ALFAHAM", price: "460/-", img: "/src/assets/Ellipse 1.png" },
+    { name: "KUZHI MANDHI", price: "800/-", img: "/src/assets/Ellipse 2.png" },
+    { name: "CHICKEN BIRIYANI", price: "140/-", img: "/src/assets/Ellipse 3.png" },
+    { name: "SHAWARMA", price: "110/-", img: "/src/assets/Ellipse 1.png" },
+    { name: "BEEF POLLICHATHU", price: "180/-", img: "/src/assets/Ellipse 5.png" },
+    { name: "FISH POLLICHATHU", price: "Seasonal", img: "/src/assets/Ellipse 6.png" }
+  ];
+
   return (
-    <Section className="" center id="menu">
-      <h2 className="font-serif text-3xl text-black italic">Menu</h2>
+    <Section className="center" id="menu">
+      <h1 className="font-lily text-6xl text-shadow-2xs">- Our Menu -</h1>
+      <div className="bg-black rounded-3xl p-10 grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-5xl">
+        {items.map((item, index) => (
+          <div key={index} className="flex items-center gap-5 text-white">
+            <img src={item.img} alt={item.name} className="w-20 h-20 rounded-full object-cover"/>
+            <div>
+              <h3 className="font-semibold text-lg">{item.name}</h3>
+              <p className="text-sm opacity-80">{item.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className="mt-10 bg-white text-black px-6 py-2 rounded-full font-medium hover:scale-105 transition">Discover entire Menu</button>
     </Section>
   );
 }
